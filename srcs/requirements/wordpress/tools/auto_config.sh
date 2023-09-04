87eafe12acfe76e0153c15d20941bbd5dbd7122a
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-09-04 12:00:44 -0400

    Update wordpress container
    - Delete conf
    - Update script

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -9,3 +9,12 @@ wp config create --allow-root \
 	--dbpass=$SQL_PASSWORD \
 	--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
+wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN} \
+	--admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email
+
+wp user create --allow-root ${WP_USER} ${WP_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf && mkdir /run/php
+
+exec php-fmp7.3 -F
+
